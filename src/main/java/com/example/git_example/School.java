6b2779e0def3d30ec2ba628ba0e2c436859d8212
sivaainkaran <ainkaransiva2@gmail.com>
2026-05-05 15:29:46 +0530

    Student Attribute

---
+++ src/main/java/com/example/git_example/School.java
@@ -0,0 +1,4 @@
+package com.example.git_example;
+
+public class School {
+}
---
+++ src/main/java/com/example/git_example/Student.java
@@ -0,0 +1,8 @@
+package com.example.git_example;
+
+public class Student {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
