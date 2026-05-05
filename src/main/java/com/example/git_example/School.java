a323aa510c7c4b1a29f276123370120a1c0e3e37
RishanaS <rizviranjan2001@gmail.com>
2026-05-05 15:46:31 +0530

    School class

--- src/main/java/com/example/git_example/School.java
+++ src/main/java/com/example/git_example/School.java
@@ -1,4 +1,24 @@
 package com.example.git_example;
 
+import jakarta.persistence.Id;
+
 public class School {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
